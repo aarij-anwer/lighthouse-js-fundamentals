1601ad9e6d06dc23febed690f1890f639c635e61
Aarij Anwer <aarij.anwer@gmail.com>
2022-09-30 23:40:30 -0400

    hello

--- tinkering.js
+++ tinkering.js
@@ -1,4 +1,4 @@
-let counter = 60;
+/*let counter = 60;
 let message = '';
 
 while (counter >= 0) {
@@ -18,13 +18,13 @@ while (counter >= 0) {
   }
   console.log(message);
   counter--;
-}
+}*/
 
 /*var x = 9;
 while (x >= 1) {
   console.log("hello " + x);
   x = x - 1;
-}*/
+}
 
 for (var x = 9; x>0; x--) {
   console.log("hello " + x);
@@ -47,4 +47,71 @@ for (let i=0; i<26; i++) {
   for (let j=0; j<100; j++) {
     console.log(i + '-' + j);
   }
+}*/
+
+/*function laugh(num) {
+  let answer = '';
+  for (let i = 0; i < num; i++) {
+    answer += 'ha';
+  }
+  return answer + '!';
 }
+
+console.log(laugh(3));*/
+/*
+function makeLine(length) {
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+      line += "* ";
+  }
+  return line + "\n";
+}
+
+function buildTriangle(length) {
+  for (let i = 1; i <= length; i++) {
+    console.log(makeLine(i));
+  }
+}
+
+buildTriangle(7);
+
+function greeting(name) {
+  console.log(`Hello, ${name}`);
+}
+
+function processUserInput(callback) {
+  const name = "Aarij";
+  //callback(name);
+  greeting(name);
+}
+
+processUserInput(greeting);*/
+/*
+var laugh = function(num) { 
+  let answer = "";
+  for (let i=0; i<num; i++) {
+    answer += 'ha';
+  }
+  return answer + '!';
+}
+
+console.log(laugh(10));
+
+var cry = function(){ 
+    return "boohoo!";
+};
+
+cry();
+*/
+
+// don't change this code
+// emotions() function definition
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// Call the emotions() function with two arguments
+// Argument 1 - "happy" string
+// Argument 2 - an inline function expression
+
